ac9b13ce9c6584f6c4086c38cf9d4835ce2b720e
Jeff Patterson <jeff_patterson@outlook.com>
2024-11-19 06:21:32 -0600

    The start of the Todo app.

---
+++ Todoey/TodoListViewController.swift
@@ -0,0 +1,47 @@
+//
+//  ViewController.swift
+//  Todoey
+//
+//  Created by Philipp Muellauer on 02/12/2019.
+//  Copyright © 2019 App Brewery. All rights reserved.
+//
+
+import UIKit
+
+class TodoListViewController: UITableViewController {
+
+    let itemArray = ["Find Mike", "Buy Eggos", "Destroy Demogorgon"]
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+    // MARK - Tableview Datasource Methods
+    
+    /// Creates the 3 default cells created by the itemArray assignment.
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return itemArray.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ToDoItemCell", for: indexPath)
+        cell.textLabel?.text = itemArray[indexPath.row]
+        return cell
+        
+    }
+    
+    //MARK - TableView Delegate Methods
+    
+    /// Triggers an event to print out the name on the cell the user selected.
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        print("You selected \(itemArray[indexPath.row])")
+        
+        /// This creates an effect that when the user presses a cell, it gets highligted and then when
+        /// released the cell is deselected. Showing an interestg effect.
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
+
+
+}
+
