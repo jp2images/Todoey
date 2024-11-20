587c4134b429a25a03e97739f5dce7791553ce95
Jeff Patterson <jeff_patterson@outlook.com>
2024-11-20 06:17:01 -0600

    End of lesson 236.

--- Todoey/TodoListViewController.swift
+++ Todoey/TodoListViewController.swift
@@ -10,11 +10,10 @@ import UIKit
 
 class TodoListViewController: UITableViewController {
 
-    let itemArray = ["Find Mike", "Buy Eggos", "Destroy Demogorgon"]
+    var itemArray = ["Find Mike", "Buy Eggos", "Destroy Demogorgon"]
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
     }
     
     // MARK - Tableview Datasource Methods
@@ -37,11 +36,49 @@ class TodoListViewController: UITableViewController {
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         print("You selected \(itemArray[indexPath.row])")
         
+        /// Add a check mark to the selected cell.
+        if tableView.cellForRow(at: indexPath)?.accessoryType == .checkmark {
+            tableView.cellForRow(at: indexPath)?.accessoryType = .none
+        }
+        else{
+            tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
+        }
+        
         /// This creates an effect that when the user presses a cell, it gets highligted and then when
         /// released the cell is deselected. Showing an interestg effect.
         tableView.deselectRow(at: indexPath, animated: true)
     }
 
-
+        // MARK - Add New Items
+    
+    @IBAction func addButtonPressed(_ sender: UIBarButtonItem) {
+        var textField = UITextField()
+        
+        let alert = UIAlertController(title: "Add New Todoey Item", message: nil, preferredStyle: .alert)
+        
+        let action = UIAlertAction(title: "Add Item", style: .default) { (action) in
+            /// What happens when the user adds item.
+            print("\(textField.text ?? "")")
+            /// A text filed will NEVER == nil. So we can safely force unwrap it.
+            /// can add validation code to make sure tha tit isn't empty. SHould alert the user and
+            /// preven adding an empty item.
+            self.itemArray.append(textField.text!)
+            self.tableView.reloadData()
+        }
+        
+        alert.addTextField { (alertTextField) in
+            alertTextField.placeholder = "Create New Item"
+            textField = alertTextField
+            /// Never prints anything from the next statement because it happens too early and there
+            /// is no value in the string variable.
+            //print(alertTextField.text)
+            //print("Now")
+        }
+        alert.addAction(action)
+        present(alert, animated: true, completion: nil)
+        
+        
+    }
+    
 }
 
