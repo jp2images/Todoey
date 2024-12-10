3fd1ac01f0ff4dfa939d23a43793c8c8fa1d5d2f
Jeff Patterson <jeff_patterson@outlook.com>
2024-12-10 06:19:05 -0600

    Beginning lesson 256

--- Todoey/Controllers/TodoListViewController.swift
+++ Todoey/Controllers/TodoListViewController.swift
@@ -11,6 +11,8 @@ import CoreData
 
 class TodoListViewController: UITableViewController {
 
+    @IBOutlet weak var searchBar: UISearchBar!
+    
     var itemArray = [Item]()
     //let dataFilePath = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)
     let context = (UIApplication.shared.delegate as!
@@ -18,10 +20,11 @@ class TodoListViewController: UITableViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        searchBar.text = "" //ERROR HERE on startup because it is NIL
         /// Path to the simulator's data location
         print(FileManager.default.urls(for: .documentDirectory,
                                        in: .userDomainMask))
+        searchBar.delegate = self
         loadItems()
     }
     
@@ -55,11 +58,20 @@ class TodoListViewController: UITableViewController {
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         //print("You selected \(itemArray[indexPath.row])")
         
-        /// To Update existing items. Wwe do something like:
-        ///itemArray[indexPath.row].setValue(value(forKey: <#T##String#>))
+        /// To Update existing items. We do something like:
+        //itemArray[indexPath.row].setValue(value(forKey: StringValueToChange))
+
+        /// To remove an item from the todo list. Order matters for this function. The item must remain at
+        /// at the correct array index when updating the context otherwise the index will be out of sync
+        /// with the item array that is displayed to the user.
+        //context.delete(itemArray[indexPath.row])
+        //itemArray.remove(at: indexPath.row)
+        
         
         /// Toggle the isComplete property for each
         itemArray[indexPath.row].isComplete = !itemArray[indexPath.row].isComplete
+        
+        
         saveItems()
         
         /// This creates an effect that when the user presses a cell, it gets highligted and then when
@@ -105,6 +117,7 @@ class TodoListViewController: UITableViewController {
         self.tableView.reloadData()
     }
     
+    /// Read the existing items into the application
     func loadItems() {
         /// One of the few areas in Swift that we must specify the data type for the entity.
         let request: NSFetchRequest<Item> = Item.fetchRequest<Item>()
@@ -115,6 +128,14 @@ class TodoListViewController: UITableViewController {
             print("Error fetching items from context, \(error)")
         }
     }
+
+}
+
+//MARK: - Search Bar Methods
+extension TodoListViewController: UISearchBarDelegate {
     
-    
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        let request: NSFetchRequest<Item> = Item.fetchRequest()
+        print(searchBar.text)
+    }
 }
