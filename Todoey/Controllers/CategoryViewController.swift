c3f8e881d843e3a8e1921c5fac48e0532793493b
Jeff Patterson <jeff_patterson@outlook.com>
2024-12-16 06:22:55 -0600

    Lesson 258 complete. Prepping for the challenge in 259.

---
+++ Todoey/Controllers/CategoryViewController.swift
@@ -0,0 +1,45 @@
+//
+//  CategoryViewController.swift
+//  Todoey
+//
+//  Created by Jeff Patterson on 12/16/24.
+//  Copyright © 2024 App Brewery. All rights reserved.
+//
+
+import UIKit
+import CoreData
+
+class CategoryViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    
+ 
+    }
+    
+    //MARK: - TableView Datasource Methods
+    
+    
+    //MARK: - TableView Delegate Methods
+    
+    //MARK: - Add Ne Categories
+    
+ 
+    @IBAction func AddButtonPressed(_ sender: UIBarButtonItem) {
+    }
+    
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 0
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return 0
+    }
+
+    
+    //MARK: - Data Manipulation Methods
+    
+}
